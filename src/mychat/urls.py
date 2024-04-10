2eb5094635824bb040bb35d27a9d7736db64e3e7
Maximilian Wagner <mwagner@students.42wolfsburg.de>
2024-04-10 22:51:57 +0200

    first commit

---
+++ src/mychat/urls.py
@@ -0,0 +1,12 @@
+# mychat/urls.py
+"""
+url patterns
+"""
+
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("chat/", include("chat.urls")),
+    path("admin/", admin.site.urls),
+]
